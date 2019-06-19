e8e21bb0f2cbdee6e01022cd791df8aa5ec6e271
Diego <dfcastap@gmail.com>
2019-06-19 14:29:40 +0200

    adds a mandatory docstring

--- petpy/petpy.py
+++ petpy/petpy.py
@@ -5,6 +5,9 @@ def gardner(vp,alpha=310,beta=0.25):
     return alpha*vp*beta
 
 def impedance(vp,rho):
+    """
+    This function calculates an impedance value
+    """
     ai = vp*rho
     return ai
 
