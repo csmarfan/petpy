2041a6b446772177958be7d355d389bb96345ebc
Fan <Yuanzhong.Fan@shell.com>
2019-06-19 07:39:54 -0500

    rho1

--- petpy/petpy.py
+++ petpy/petpy.py
@@ -4,7 +4,7 @@ petrophysics equations
 def gardner(vp,alpha=310,beta=0.25):
     return alpha*vp*beta
 
-def impedance(vp,rho):
-    ai = vp*rho
+def impedance(vp,rho1):
+    ai = vp*rho1
     return ai
 
